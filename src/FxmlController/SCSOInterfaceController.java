8c8a45c5f239db210f1abfeec7fc8df65acdec7d
Marbella91 <marion.bellefin@gmail.com>
2015-10-10 11:45:31 +0200

    generates the list of authorized event planning requests for an employee

--- src/FxmlController/SCSOInterfaceController.java
+++ src/FxmlController/SCSOInterfaceController.java
@@ -1,12 +1,16 @@
 package FxmlController;
 
 import java.net.URL;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import javax.swing.JOptionPane;
 
 import EventPlanningRequest.EventPlanningRequest;
 import Login.Employee;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -17,14 +21,13 @@ import javafx.scene.control.TextField;
 
 public class SCSOInterfaceController  implements Initializable{
 	private Employee employee;
-	private double recordNumber;
-	private String clientName;
-	private String eventType;
+	private LinkedList<EventPlanningRequest> EPRequestsList;
 	@FXML private Label labelLogin;
-	@FXML private ListView listView;
+	@FXML private ListView labelPendingCommentsRequests;
 	
 	public SCSOInterfaceController(Employee employee){
 		this.employee=employee;
+		this.EPRequestsList = EventPlanningRequest.generateAuthorizedEPRequestsList(employee);
 	}
 	
 	@Override
@@ -32,7 +35,7 @@ public class SCSOInterfaceController  implements Initializable{
 		// TODO Auto-generated method stub
 		this.labelLogin.setText(this.employee.getLogin());
 		//List<String> values = Arrays.asList("one", "two", "three");
-		//listView.setItems(FXCollections.observableList(values));
+		//labelPendingCommentsRequests.setItems(FXCollections.observableList(values));
 	}
 	
 	 
